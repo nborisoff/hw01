b0a703a982fac402d3675111af511e38abc0fa70
nborisoff <nborissov256@gmail.com>
2024-03-11 00:42:04 +0300

    #fix homepage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { Request, Response } from "express";
 import { SETTINGS } from "./app/settings";
 import { app } from "./app/app";
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello samurai");
+});
 app.listen(SETTINGS.PORT, () => {
   console.log(`App listening on port ${SETTINGS.PORT}`);
 });
